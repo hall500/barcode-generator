e70cff3c1cd558165b33b835862f7c693e4f5c4f
Abdullah Momoh <momohabdullah20@gmail.com>
2023-11-28 08:22:33 +0100

    updated barcode generator

--- generate.js
+++ generate.js
@@ -3,6 +3,9 @@ const uuid = require('uuid');
 const axios = require('axios');
 const path = require('path');
 const Jimp = require('jimp');
+const yargs = require('yargs');
+const { Readable } = require('stream');
+const csv = require('csv-parser');
 
 // Define the output folder path
 const outputFolderPath = path.join(__dirname, 'barcodes');
@@ -26,17 +29,6 @@ function deleteFolderRecursive(folderPath) {
     }
 }
 
-// Delete the "barcodes" folder and CSV file if they exist
-deleteFolderRecursive(outputFolderPath);
-if (fs.existsSync(csvFilePath)) {
-    fs.unlinkSync(csvFilePath);
-}
-
-// Create the output folder if it doesn't exist
-if (!fs.existsSync(outputFolderPath)) {
-    fs.mkdirSync(outputFolderPath);
-}
-
 const guid = () => {
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
         let r = (Math.random() * 16) | 0,
@@ -55,25 +47,29 @@ const generateOrderId = (length = 14) => {
     return result;
 };
 
-const addTextToBarcodeImage = async (barcodeImage, text) => {
+const addTextToBarcodeImage = async (image) => {
+    var fileName = image;
+    var imageCaption = image.split('/').pop().split('_')[0];
+    console.log(imageCaption);
+    var loadedImage;
+
     // Load the barcode image
-    const image = await Jimp.read(barcodeImage);
-  
-    // Create a text object
-    const textObject = new Jimp.Text({
-      text,
-      alignment: Jimp.ALIGN_RIGHT,
-      position: {
-        x: image.bitmap.width - 10,
-        y: 10,
-      },
+    Jimp.read(fileName)
+    .then(function (image) {
+        loadedImage = image;
+        return Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+    })
+    .then(function (font) {
+        // Calculate the position to place the text at the bottom right
+        var x = loadedImage.bitmap.width - Jimp.measureText(font, imageCaption);
+        var y = loadedImage.bitmap.height - 20; // You can adjust the value for vertical positioning
+
+        loadedImage.print(font, x, y, imageCaption)
+                   .write(fileName);
+    })
+    .catch(function (err) {
+        console.error(err);
     });
-  
-    // Add the text to the image
-    image.composite(textObject, 0, 0);
-  
-    // Save the image
-    await image.writeAsync('output.png');
 }
 
 const getQRCode = (text) => {
@@ -97,21 +93,50 @@ const getQRCode = (text) => {
     return `${baseUrl}?${queryParams}`;
 }
 
+function listFilesInFolderStream(folderPath) {
+    const readStream = new Readable({
+      objectMode: true,
+      read() {},
+    });
+  
+    // Read the contents of the folder and push file names to the stream
+    fs.readdir(folderPath, (err, files) => {
+      if (err) {
+        console.error('Error reading folder:', err);
+        return readStream.push(null);
+      }
+  
+      files.forEach(file => {
+        readStream.push(file);
+      });
+  
+      readStream.push(null); // Signal the end of the stream
+    });
+  
+    readStream.on('data', file => {
+      addTextToBarcodeImage(`${folderPath}/${file}`); 
+    });
+  
+    readStream.on('end', () => {
+      console.log('Finished listing files in the folder.');
+    });
+  }
+
 // Function to generate UUIDs and barcodes
 // Function to generate UUIDs and barcodes
-async function generateUUIDsAndBarcodes(count) {
+async function generateUUIDsAndBarcodes(start = 0, count = 1000) {
     const uuidData = [];
 
-    for (let i = 0; i < count; i++) {
+    const end = start + count;
+    for (let i = start; i <= end; i++) {
         const code_id = guid();
-        const num = i + 1;
+        const num = (start > 0) ? i : i + 1;
         //const code_url = `https://rider.transpay.com/${code_id}?q=${num}`;
-        const code_url = `https://transpay.vercel.app/search`;
+        const code_url = `https://transpay.vercel.app/barcode/${code_id}`;
         const qrCodeUrl = getQRCode(code_url);
 
-        uuidData.push({
+        if(i < count) uuidData.push({
             id: num,
-            item_id: generateOrderId(20),
             code_id,
             code_url,
         });
@@ -121,31 +146,93 @@ async function generateUUIDsAndBarcodes(count) {
         const qrCodeFilePath = path.join(outputFolderPath, `${num}_qr.png`);
 
         qrCodeResponse.data.pipe(fs.createWriteStream(qrCodeFilePath));
-
-        // Add text to the QR code image
-        const textToAdd = `T${num}`;
-        const barcodeOutputFilePath = path.join(outputFolderPath, `${num}_barcode.png`);
-        //await addTextToBarcodeImage(qrCodeFilePath, textToAdd, barcodeOutputFilePath); 
-
-        // Save the final barcode image path in uuidData
-        uuidData[num - 1].barcode_url = barcodeOutputFilePath;
     }
 
     return uuidData;
 }
 
+function csvToSqlInsert(csvFilePath, tableName) {
+    const inserts = [];
+  
+    fs.createReadStream(csvFilePath)
+      .pipe(csv())
+      .on('data', (row) => {
+        const values = Object.values(row)
+          .map(value => typeof value === 'string' ? `'${value}'` : value)
+          .join(', ');
+  
+        inserts.push(`INSERT INTO ${tableName} VALUES (${values});`);
+      })
+      .on('end', () => {
+        // Save the SQL inserts to a file or do something else with them
+        fs.writeFileSync('output.sql', inserts.join('\n'));
+        console.log('Conversion completed. Check output.sql for SQL inserts.');
+      });
+  }
 
 // Generate and write UUIDs and barcodes to CSV
 async function main() {
+    // Delete the "barcodes" folder and CSV file if they exist
+    deleteFolderRecursive(outputFolderPath);
+    if (fs.existsSync(csvFilePath)) {
+        fs.unlinkSync(csvFilePath);
+    }
+
+    // Create the output folder if it doesn't exist
+    if (!fs.existsSync(outputFolderPath)) {
+        fs.mkdirSync(outputFolderPath);
+    }
+
     const uuidCount = 2;
-    const uuidData = await generateUUIDsAndBarcodes(uuidCount);
+    const start = parseInt(argv.start) || 0;
+    const count = parseInt(argv.count) || 1000;
+    const uuidData = await generateUUIDsAndBarcodes(start, count);
 
     // Write UUID data to a CSV file
-    const csvData = uuidData.map((item) => `${item.id},${item.code_id},${item.code_id},${item.code_url}`).join('\n');
-    fs.writeFileSync(csvFilePath, `id,item_id,code_id,barcode_url\n${csvData}`);
+    const csvData = uuidData.map((item) => `${item.id},${item.code_id},${item.code_url}`).join('\n');
+
+    fs.writeFileSync(csvFilePath, `id,code_id,barcode_url\n${csvData}`);
 
     console.log(`Generated ${uuidCount} UUIDs and barcodes.`);
     process.exit();
 }
 
-main().catch((err) => console.error(err));
+// Define command line options
+const argv = yargs
+  .option('start', {
+    alias: 's',
+    description: 'Number to start generating barcodes from',
+    type: 'number',
+  })
+  .option('count', {
+    alias: 'c',
+    description: 'Number of barcodes to generate',
+    type: 'number',
+  })
+  .option('generate', {
+    alias: 'g',
+    description: 'Generate barcode. use values image or text or both e.g image,text',
+    type: 'string',
+  })
+  .help()
+  .argv;
+
+const generate = argv.generate.split(',');
+
+if(generate.length > 1) {
+  console.error('Cannot run more than one command at a time');
+}
+
+if(generate.includes('image')){
+    main().catch((err) => console.error(err));
+}
+
+if(generate.includes('text')) listFilesInFolderStream('barcodes');
+
+if(generate.includes('sql')){
+    // Example usage:
+    const filepath = csvFilePath; // Replace with your CSV file path
+    const tableName = 'transpay_barcodes'; // Replace with your table name
+
+    csvToSqlInsert(filepath, tableName);
+}
